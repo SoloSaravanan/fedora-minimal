a52b781a90c4012e50f55e569cb3eac90c2922f1
SoloSaravanan <SoloSaravanan@proton.me>
2024-12-05 22:33:17 +0530

    fedora: very minimal

--- fedoraminimal.sh
+++ fedoraminimal.sh
@@ -2,8 +2,10 @@
 
 # DNF Config
 sudo dnf config-manager setopt installonly_limit=2
-sudo dnf config-manager setopt max_parallel_downloads=10
+sudo dnf config-manager setopt max_parallel_downloads=15
 sudo dnf config-manager setopt fastestmirror=1
+
+sudo dnf upgrade --refresh
 
 # RPM Fusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
@@ -11,13 +13,15 @@ sudo dnf config-manager --enable fedora-cisco-openh264
 sudo dnf update @core
 sudo dnf -y upgrade --refresh
 
-# Firmwares
-sudo dnf install alsa-sof-firmware intel-gpu-firmware linux-firmware linux-firmware-whence mt7xxx-firmware nvidia-gpu-firmware realtek-firmware
-
 # KDE
-sudo dnf install plasma-workspace-wayland sddm-wayland-plasma sddm-breeze plasma-nm
-sudo dnf install bash-completion konsole firefox gwenview dolphin ffmpegthumbs kate spectacle kde-gtk-config kscreen sddm-kcm firewall-config NetworkManager-wifi NetworkManager-bluetooth bluedevil --setopt=install_weak_deps=false
+sudo dnf install plasma-workspace-wayland sddm-wayland-plasma sddm-breeze plasma-nm plasma-pa --setopt=install_weak_deps=false
+
+# KDE tools
+sudo dnf install bash-completion konsole firefox gwenview dolphin ffmpegthumbs kate spectacle kde-gtk-config kscreen sddm-kcm firewall-config NetworkManager-wifi NetworkManager-bluetooth bluedevil upower tuned-ppd  --setopt=install_weak_deps=false
 sudo dnf in ark
+
+# Firmwares
+sudo dnf install alsa-sof-firmware intel-gpu-firmware mt7xxx-firmware nvidia-gpu-firmware realtek-firmware
 
 # Fonts
 sudo dnf group install fonts
@@ -26,6 +30,9 @@ sudo dnf group install fonts
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing
 sudo dnf group install multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf install intel-media-driver
+
+sudo systemctl --user --now enable wireplumber.service
+sudo systemctl --user --now enable pipewire
 
 # SDDM
 sudo systemctl set-default graphical.target
