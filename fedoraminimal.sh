8ec6a799c9f50c166b49c4103997cd60c9fe79e2
SoloSaravanan <SoloSaravanan@proton.me>
2024-12-10 15:29:25 +0530

    install packages with -y

--- fedoraminimal.sh
+++ fedoraminimal.sh
@@ -5,31 +5,30 @@ sudo dnf config-manager setopt installonly_limit=2
 sudo dnf config-manager setopt max_parallel_downloads=15
 sudo dnf config-manager setopt fastestmirror=1
 
-sudo dnf upgrade --refresh
+sudo dnf -y upgrade --refresh
 
 # RPM Fusion
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf config-manager --enable fedora-cisco-openh264
-sudo dnf update @core
+sudo dnf -y update @core
 sudo dnf -y upgrade --refresh
 
 # KDE
-sudo dnf install plasma-workspace-wayland sddm-wayland-plasma sddm-breeze plasma-nm plasma-pa --setopt=install_weak_deps=false
+sudo dnf -y install plasma-workspace-wayland sddm-wayland-plasma sddm-breeze plasma-nm plasma-pa --setopt=install_weak_deps=false
 
 # KDE tools
-sudo dnf install bash-completion konsole firefox gwenview dolphin ffmpegthumbs kate spectacle kde-gtk-config kscreen sddm-kcm firewall-config NetworkManager-wifi NetworkManager-bluetooth bluedevil upower tuned-ppd  --setopt=install_weak_deps=false
-sudo dnf in ark
+sudo dnf -y install bash-completion konsole firefox gwenview dolphin ffmpegthumbs kate spectacle kde-gtk-config kscreen sddm-kcm firewall-config NetworkManager-wifi NetworkManager-bluetooth bluedevil upower tuned-ppd  --setopt=install_weak_deps=false
+sudo dnf -y install ark
 
 # Firmwares
-sudo dnf install alsa-sof-firmware intel-gpu-firmware mt7xxx-firmware nvidia-gpu-firmware realtek-firmware
+sudo dnf -y install mt7xxx-firmware realtek-firmware
 
 # Fonts
-sudo dnf group install fonts
+sudo dnf -y group install fonts
 
 # Multimedia
-sudo dnf swap ffmpeg-free ffmpeg --allowerasing
-sudo dnf group install multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-sudo dnf install intel-media-driver
+sudo dnf -y swap ffmpeg-free ffmpeg --allowerasing
+sudo dnf -y group install multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 
 sudo systemctl --user --now enable wireplumber.service
 sudo systemctl --user --now enable pipewire
