48149eefada576e67669cce610246ea2d173029c
SoloSaravanan <SoloSaravanan@proton.me>
2025-01-15 11:37:07 +0530

    Enable openh264

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sudo dnf -y upgrade --refresh
 
 # RPM Fusion
 sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf config-manager --enable fedora-cisco-openh264
+sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
 sudo dnf -y update @core
 sudo dnf -y upgrade --refresh
 
