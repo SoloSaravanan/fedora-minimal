6e1e274387f7a7a82ff433e80a5be55188f33a7c
SoloSaravanan <SoloSaravanan@proton.me>
2026-02-20 08:31:12 +0530

    add some nvidia modprobe conf

--- drivers.sh
+++ drivers.sh
@@ -28,11 +28,14 @@ if lspci | grep -Ei 'nvidia|nvidia corporation' > /dev/null 2>&1; then
     sudo tee /etc/modprobe.d/nvidia.conf > /dev/null << 'EOF'
 blacklist nouveau
 blacklist nova
+blacklist nova_core
 options nouveau modeset=0
 options nvidia-drm modeset=1 fbdev=1
 options nvidia NVreg_UsePageAttributeTable=1 \
     NVreg_InitializeSystemMemoryAllocations=0 \
-    NVreg_DynamicPowerManagement=0x03
+    NVreg_DynamicPowerManagement=0x03 \
+    NVreg_EnableS0ixPowerManagement=1 \
+    NVreg_RegistryDwords=RmEnableAggressiveVblank=1
 EOF
 
     # Create NVIDIA udev rules
@@ -43,6 +46,8 @@ ACTION=="add|bind", SUBSYSTEM=="pci", DRIVERS=="nvidia", ATTR{vendor}=="0x10de",
 # Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
 ACTION=="remove|unbind", SUBSYSTEM=="pci", DRIVERS=="nvidia", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", TEST=="power/control", ATTR{power/control}="on"
 EOF
+
+sudo sed -i '/^enabled=1$/!b; n; /^[[:space:]]*exclude=/!i exclude=*nvidia*' /etc/yum.repos.d/rpmfusion-nonfree*.repo
 
     # Add NVIDIA driver repository
     sudo tee -a /etc/yum.repos.d/nvidia-driver.repo > /dev/null << 'EOF'
@@ -56,7 +61,7 @@ EOF
 
     #  Install NVIDIA driver
     sudo dnf install -y nvidia-cfg nvidia-common nvidia-cuda nvidia-driver nvidia-egl nvidia-egl-gbm nvidia-egl-wayland nvidia-gbm nvidia-gles nvidia-glx nvidia-gpu-firmware nvidia-modprobe nvidia-modules-open nvidia-ngx nvidia-opencl nvidia-powerd nvidia-security nvidia-settings nvidia-smi nvidia-vdpau nvidia-video nvidia-vision
-
+    sudo dnf install -y libva-nvidia-driver
     #Enable Nvidia suspend, dynamic boost
     sudo systemctl enable nvidia-{suspend,resume,hibernate,suspend-then-hibernate}
     sudo cp /usr/share/dbus-1/system.d/nvidia-dbus.conf /etc/dbus-1/system.d/
